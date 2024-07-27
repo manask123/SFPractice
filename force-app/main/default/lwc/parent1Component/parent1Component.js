5933aebf7b5fb24dcf9e0e708371bb901163c418
Manas <manas.kamarapu@gmail.com>
2024-07-27 11:38:02 -0400

    part1

--- force-app/main/default/lwc/parent1Component/parent1Component.js
+++ force-app/main/default/lwc/parent1Component/parent1Component.js
@@ -2,8 +2,7 @@ import { LightningElement, api } from 'lwc';
 
 export default class Parent1Component extends LightningElement {
   @api p1v = 'Manas';
-
-  p1InputVal = '';
+  p1InputVal = '';  //giving error if it is @api
 
   handleChange(event) {
     this.p1InputVal = event.target.value;
